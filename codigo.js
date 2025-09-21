7ece77753eda9800173321d0bdee8f1b0746dfbd
ZdasienStefano <styzdasien@gmail.com>
2025-09-21 15:21:25 -0300

    Portfolio Stefano Zdasien

---
+++ codigo.js
@@ -0,0 +1,8 @@
+    function saludar(){
+
+        let nombre = document.getElementById("idNombre").value;
+        let correo = document.getElementById("idCorreo").value;
+        let mensaje = document.getElementById("idMensaje").value;
+
+        alert(nombre + " ya se envio tu mensaje, Gracias!!");
+    }
